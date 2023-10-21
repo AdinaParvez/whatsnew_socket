f01800e4b69df4423931670d4e7b9ac004fa3d4f
AdinaParvez <81682886+AdinaParvez@users.noreply.github.com>
2023-10-21 22:46:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server(9000, {
     cors: {
-        origin: 'http://localhost:3000' || 'https://653361ecbfae0c37e94fc717--aquamarine-muffin-72bae9.netlify.app/'
+        origin: 'https://653407b8598cac168b7894c7--dashing-starburst-d08dcf.netlify.app/'
     }
 })
 //to show active users below
